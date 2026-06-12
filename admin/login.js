b9ac62dce442c071253865a65fd64f1253f5be5f
cmeg <cmexpertsgroup@gmail.com>
2026-06-12 05:45:23 +0530

    Create login.js

---
+++ admin/login.js
@@ -0,0 +1,68 @@
+import {
+collection,
+getDocs
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+async function login(){
+
+const username =
+document.getElementById(
+"username"
+).value.trim();
+
+const password =
+document.getElementById(
+"password"
+).value.trim();
+
+const users =
+await getDocs(
+collection(
+window.db,
+"users"
+)
+);
+
+let validUser = null;
+
+users.forEach(doc=>{
+
+const user = doc.data();
+
+if(
+
+user.username === username &&
+user.password === password &&
+user.active === true
+
+){
+
+validUser = user;
+
+}
+
+});
+
+if(!validUser){
+
+alert(
+"Invalid Username or Password"
+);
+
+return;
+
+}
+
+localStorage.setItem(
+"currentUser",
+JSON.stringify(validUser)
+);
+
+window.location.href =
+"index.html";
+
+}
+
+window.login = login;
